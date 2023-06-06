68e86f7aae3ef91a6460ba9725806d20e889cc62
bilbo <bilbo.derbyshire@gmail.com>
2023-06-06 15:43:13 +0100

    Feature: Created settings page with app geometry and title

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import tkinter as tk
 import requests
 import json
 import datetime
+from settings import *
 
 
 class WeatherApp(tk.Tk):
     def __init__(self):
         super().__init__()
+
+        self.geometry(GEOMETRY)
+        self.title(ROOT_TITLE)
 
 
 if __name__ == "__main__":
